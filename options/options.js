2a116807444c92d75c3bda1f99b9042cc637ace5
Crowdbotics <team@crowdbotics.com>
2021-11-19 00:56:24 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "frontline_safety_32038",
+  url: "https://frontline_safety_32038.botics.co",
+  api: "https://frontline_safety_32038.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
